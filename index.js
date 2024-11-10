a0ab9065a4dfb93503a070c1716db42a382b05d1
Leo Pinho <leonardo.zoot@gmail.com>
2024-11-10 00:19:43 -0300

    operand functions done

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ function divide(x, y) {
     return x / y
 }
 
-let firstParameter, secondParameter, currentOperator
+let firstOperand = '', currentOperator = '', secondOperand = ''
+const refreshDisplay = () => {
+    display.textContent = `${firstOperand} ${currentOperator} ${secondOperand}`
+}
 
 function operate(operandX, operandY, operator) {
     if(operator === add){
@@ -33,9 +36,36 @@ const display = document.querySelector('.display')
 
 const numericDigits = document.querySelectorAll('.base-btn')
 
+//feeds digits into variable and display
 numericDigits.forEach(button => {
     const buttonContent = button.textContent
     button.addEventListener('click', function() {
-        display.textContent = buttonContent
+        //if operator is not set yet, add digit to firstOperand
+        if(currentOperator === ''){
+            firstOperand += buttonContent
+            refreshDisplay()
+        } else if(currentOperator !== '') {
+            secondOperand += buttonContent
+            refreshDisplay()
+        }
     })
 });
+
+//add function to operator digits
+const operatorDigits = document.querySelectorAll('.operator-btn')
+operatorDigits.forEach(button => {
+    const operatorButtonContent = button.textContent
+    button.addEventListener('click', function() {
+        currentOperator.textContent = operatorButtonContent
+        refreshDisplay()
+    })
+})
+
+
+//step 5
+// add digits to first operand
+//when operator button is clicked, previous operand is fed into a variable 
+//operator is fed into a variable
+//when another digit is clicked after operator has already been chosen, a third variable will be the target of the new digits
+//add function to operator digits
+//clicking on '=' call the desired function based on the operator variable
